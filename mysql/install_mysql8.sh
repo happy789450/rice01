fb2d216e88b50cf85f38406f8d47e60c965bf903
Your Name <you@example.com>
2025-02-26 00:13:48 +0800

    v2.2.77 update mysql

--- mysql/install_mysql8.sh
+++ mysql/install_mysql8.sh
@@ -9,3 +9,19 @@ sudo systemctl start mysqld
 sudo systemctl enable mysqld
 
 sudo grep 'temporary password' /var/log/mysqld.log
+tmp_pass=$(grep 'temporary password' /var/log/mysqld.log  | awk '{print $NF}' | tail -1)
+
+mysql -uroot -p$tmp_pass -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'MyNewPass123!';"
+mysql -uroot -pMyNewPass123! -e "SET GLOBAL validate_password.policy = 'LOW';"
+mysql -uroot -pMyNewPass123! -e "SET GLOBAL validate_password.length = 6;"
+mysql -uroot -pMyNewPass123! -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '123456';"
+
+cat <<EOF >>/etc/my.cnf
+validate_password.policy=low
+validate_password.length=6
+EOF
+
+systemctl restart mysqld
+systemctl status mysqld
+
+echo "mysql8 密码策略修改成功"

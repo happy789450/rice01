d3f8a6dc10dfa5aa1893251248d2eba779eccab0
Your Name <you@example.com>
2025-04-12 23:50:17 +0800

    v2.3.25 update grafana

---
+++ prometheus/grafana.sh
@@ -0,0 +1,14 @@
+cat <EOF | sudo tee /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://rpm.grafana.com
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://rpm.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+sudo yum install grafana-enterprise
+sudo systemctl start grafana-server
+sudo systemctl enable grafana-server
